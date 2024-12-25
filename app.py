2eb6c9f67791f3077863713bc69602e942ef43f0
ganesh2152 <ganeshb2152@gmail.com>
2024-12-25 22:37:35 +0530

    app added

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+data = {
+    'Course Name':['DevOps','Hadoop','PySpark'],
+    'Duration':['30 days','20 days','30 days']
+}
+
+df = pd.DataFrame(data)
+
+st.title('Course Information')
+st.write(df)
